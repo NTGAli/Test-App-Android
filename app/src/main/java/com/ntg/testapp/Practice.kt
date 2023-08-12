251fb79fc63ea37fdc966b57c264d66dc74ebea2
Ali Ntg <alintg@gmail.com>
2023-08-12 06:28:16 +0330

    fibonacci function

--- app/src/main/java/com/ntg/testapp/Practice.kt
+++ app/src/main/java/com/ntg/testapp/Practice.kt
@@ -11,7 +11,20 @@ object Practice {
      */
 
     fun fib(n: Int): Long{
-        return n.toLong()
+        if (n == 0 || n == 1){
+            return n.toLong()
+        }
+
+        var a = 0L
+        var b = 1L
+        var c = 1L
+
+        (1 until n).forEach {
+            c = a + b
+            a = b
+            b = c
+        }
+        return c
     }
 
 
