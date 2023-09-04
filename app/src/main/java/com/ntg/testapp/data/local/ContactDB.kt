0f3cddf866a1187a84035300744887f064c1a142
Ali Ntg <Alintg@outlook.com>
2023-09-04 10:51:38 +0330

    DB implemented

--- app/src/main/java/com/ntg/testapp/data/local/ContactDB.kt
+++ app/src/main/java/com/ntg/testapp/data/local/ContactDB.kt
@@ -1,4 +1,10 @@
 package com.ntg.testapp.data.local
 
+import androidx.room.Database
+
+@Database(entities = [ContactItem::class], version = 1)
 abstract class ContactDB {
+
+    abstract fun contactsDao(): ContactDao
+
 }
